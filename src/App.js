315722276d3830b6ba80660026004876dbb43d82
vijenderchimma <vijenderchimma424@gmail.com>
2023-04-25 16:04:54 +0000

    nxtTrendz authentication

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+import LoginForm from './components/LoginForm'
+import NotFound from './components/NotFound'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/" component={Home} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
